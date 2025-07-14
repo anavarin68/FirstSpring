22f84e9cfd762a29e24bffda301c47d79fc6ac24
Nitish <kumarpateln7@gmail.com>
2025-07-15 00:15:38 +0530

    Updated StudentDetails

--- src/main/java/com/example/FirstSpring/Model/StudentDetails.java
+++ src/main/java/com/example/FirstSpring/Model/StudentDetails.java
@@ -18,31 +18,39 @@ public class StudentDetails {
 	public Integer getSid() {
 		return sid;
 	}
+	
 	public void setSid(Integer sid) {
 		this.sid = sid;
 	}
+	
 	public String getSname() {
 		return sname;
 	}
+	
 	public void setSname(String sname) {
 		this.sname = sname;
 	}
+	
 	public String getScity() {
 		return scity;
 	}
+	
 	public void setScity(String scity) {
 		this.scity = scity;
 	}
+	
 	public StudentDetails(Integer sid, String sname, String scity) {
 		super();
 		this.sid = sid;
 		this.sname = sname;
 		this.scity = scity;
 	}
+	
 	public StudentDetails() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	
 	@Override
 	public String toString() {
 		return "StudentDetails [sid=" + sid + ", sname=" + sname + ", scity=" + scity + "]";
